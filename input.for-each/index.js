0d45c3bf248b3e8bd98338b08c723f7b433bd592
Stefan de Jong <steve25070@gmail.com>
2020-04-13 13:11:10 +0200

    lazy on-demand for-each processing
    Implements a 'next' input which must be triggered before the next item 
    in the 'any' input is processed.

--- input.for-each/index.js
+++ input.for-each/index.js
@@ -2,20 +2,46 @@
 
 module.exports = (NODE) => {
   const triggerIn = NODE.getInputByName('trigger');
+  const nextIn = NODE.getInputByName('next');
   const anyIn = NODE.getInputByName('any');
 
   const triggerOut = NODE.getOutputByName('trigger');
+  const doneOut = NODE.getOutputByName('done');
+
+  function next(value, state, values, index) {
+    const thisState = state.split();
+    thisState.set(NODE, {
+      value,
+      index,
+      values
+    });
+    triggerOut.trigger(thisState);
+
+    if (index + 1 >= values.length) {
+      doneOut.trigger(state);
+    }
+  }
 
   triggerIn.on('trigger', async (conn, state) => {
     const values = await anyIn.getValues(state);
 
-    values.forEach((value) => {
-      const thisState = state.split();
-      thisState.set(NODE, {
-        value
-      });
-      triggerOut.trigger(thisState);
-    });
+    if (!nextIn.isConnected()) {
+      values.forEach((value, index) => next(value, state, values, index));
+
+      return;
+    }
+
+    next(values[0], state, values, 0);
+  });
+
+  nextIn.on('trigger', async (conn, state) => {
+    const thisState = state.get(NODE);
+    if (thisState && typeof thisState.index === 'number') {
+      if (thisState.index + 1 >= thisState.values.length) {
+        return;
+      }
+      next(thisState.values[thisState.index + 1], state, thisState.values, thisState.index + 1);
+    }
   });
 
   const itemOut = NODE.getOutputByName('item');
